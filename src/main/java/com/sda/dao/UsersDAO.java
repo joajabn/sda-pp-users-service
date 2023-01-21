0d6bfba0f3d6bf821158f45f34e99dfc4835525e
Uzytkownik <uzytkownik@kursuGIT.pl>
2023-01-21 15:23:45 +0100

    UsersDAO modified

--- src/main/java/com/sda/dao/UsersDAO.java
+++ src/main/java/com/sda/dao/UsersDAO.java
@@ -5,6 +5,9 @@ import com.sda.model.User;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class UsersDAO {
 
     public void create(User user) {
@@ -26,4 +29,52 @@ public class UsersDAO {
             return user != null;
         }
     }
+
+    public List<User> findAll(){
+        Session session = HibernateUtils.getSessionFactory().openSession();
+        String query = "SELECT u FROM User u";
+        List<User> users = session.createQuery(query, User.class).list();
+        session.close();
+        return users;
+
+    }
+
+    public User findUserByUsername(String username){
+        try (Session session = HibernateUtils.getSessionFactory().openSession()) {
+            return session.find(User.class, username);
+        }
+
+    }
+
+//    public void updateUser(User user){
+//        try (Session session = HibernateUtils.getSessionFactory().openSession()) {
+//            Transaction transaction = session.beginTransaction();
+//            session.merge(user);
+//            transaction.commit();
+//        }
+//
+//    }
+
+    public User update(User updatedUser) {
+        Session session = HibernateUtils.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        User updateUser = session.merge(updatedUser);
+        transaction.commit();
+        session.close();
+        return updateUser;
+    }
+
+    public boolean existsByUsername(String username) {
+        Session session = HibernateUtils.getSessionFactory().openSession();
+        String query = "SELECT count(u) FROM User u WHERE u.username = :username";
+
+        boolean exists = session.createQuery(query, Long.class)
+                .setParameter("username", username)
+                .uniqueResult() > 0;
+
+        session.close();
+        return exists;
+    }
+
+
 }
